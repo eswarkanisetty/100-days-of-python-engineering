b0f1152c7806cf0fdcd718d08d2320057369f1e2
Eswar Kanisetty <eswarkanisetty@gmail.com>
2026-03-16 14:41:05 -0400

    feat: implement interactive CLI for string concatenation and variable management

---
+++ Day_1_CLI_String_Processing/task/task.py
@@ -0,0 +1,5 @@
+print("Welcome to the Brand Name Generator :")
+city_name = input("What is the name of the city you grew up in ? : ")
+pet_name = input("What is your pet's name ? : ")
+print("Your Brand Name Could be : " + city_name + " " + pet_name)
+
