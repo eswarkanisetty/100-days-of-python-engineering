02687522087eeebeea2aa07b45dfb398e8c77cbe
eswarkanisetty <eswarkanisetty@gmail.com>
2026-03-16 15:35:13 -0400

    Update task.py

--- Day_1_CLI_String_Processing/task/task.py
+++ Day_1_CLI_String_Processing/task/task.py
@@ -1,5 +1,4 @@
-print("Welcome to the Brand Name Generator :")
-city_name = input("What is the name of the city you grew up in ? : ")
-pet_name = input("What is your pet's name ? : ")
-print("Your Brand Name Could be : " + city_name + " " + pet_name)
-
+Create a greeting for your program.
+Ask the user for the city that they grew up in and store it in a variable.
+Ask the user for the name of a pet and store it in a variable.
+Combine the name of their city and pet and show them their band name.
